df9680e78d51113f2a73e223f1306ac5528c7564
pineapple laptop <martinkosel@yahoo.de>
2023-11-06 20:29:23 +0000

    Adds questions and logic to exchange text content on the screen with questions and answers

---
+++ assets/js/logic.js
@@ -0,0 +1,18 @@
+
+function startQuiz (timer) {
+    // Save start-screen id in a variable
+    const startScreen = document.getElementById("start-screen");
+
+    // Save start button id in a variable
+    const startButton = document.getElementById("start");
+    
+    // Add event listener to start button and change start screen
+    startButton.addEventListener("click", function () {
+        startScreen.children[0].textContent = '';
+        startScreen.children[1].textContent = 
+    });
+
+
+
+}
+startQuiz();
